0f2a2bf4ac70d78318e4a47d1265187c5e6aa850
Erfan Faridi <109587086+Erfan-ffa@users.noreply.github.com>
2022-08-24 15:58:18 +0430

    Add files via upload

---
+++ FindingTheIndexOfElementInArrayWithBinarySearch/FindingTheIndexOfElementInArrayWithBinarySearch/RecursiveFinder.cs
@@ -0,0 +1,26 @@
+﻿namespace FindingTheIndexOfElementInArrayWithBinarySearch
+{
+    public class RecursiveFinder
+    {
+        public void RecursiveFinde(int[] array, int first, int last, int key)
+        {
+            int middle = (first + last) / 2;
+            if (first == last)
+            {
+                Console.WriteLine(last);
+            }
+            else if (key == array[middle])
+            {
+                Console.WriteLine(middle + 1);
+            }
+            else if (key > array[middle])
+            {
+                RecursiveFinde(array, middle + 1, last, key);
+            }
+            else
+            {
+                RecursiveFinde(array, first, last - 1, key);
+            }
+        }
+    }
+}
